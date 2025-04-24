ff8e16615190d796aa6b15744d7e552f32baf4a0
Mirona <mironabotanel@yahoo.com>
2025-04-25 00:48:50 +0300

    matching CVs with job descriptions using NLP

---
+++ Home.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(page_title="CV & Job Matcher", layout="centered")
+
+st.title("Job Description <-> CV Matcher")
+
+st.markdown( """ Welcome! Use the sidebar to:
+1. Match a new job description to the most suitable CVs.
+2. Upload a CV and get the best-fitting job. """ )
+
+#st.progress(10)
+
+#st.spinner("Wait for it...")
+
+
+#st.balloons()
